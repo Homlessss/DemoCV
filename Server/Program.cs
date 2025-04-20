125fdcbed2f262336d2340f7f039e82115467083
Homlessss <48959447+Homlessss@users.noreply.github.com>
2025-04-20 13:53:38 +0000

    first commit

---
+++ Server/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5100); // HTTP
+    options.ListenAnyIP(7223, listenOptions => listenOptions.UseHttps()); // HTTPS
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapGet("/", () => "Welcome to the API!");
+
+app.MapControllers();
+
+app.Run();
